c6e34747dd4cba68f493beee2da9823d181d2a49
Mohamed Sambe <mohamedsambe081@gmail.com>
2026-06-17 14:28:00 +0000

    use sqlite on render

--- telecom_revenue/settings.py
+++ telecom_revenue/settings.py
@@ -60,12 +60,8 @@ ASGI_APPLICATION = "telecom_revenue.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "chinguitel_revenue"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "admin44"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
